02d6a25a8bc63f7bd8ec60d8a9f94e8ed02c5b98
Cryptocracy <admin@cryptocracy.io>
2018-03-26 00:49:29 -0700

    Basic auth with sample video
    video not sourced via gaia, but Blockstack auth does work.

---
+++ src/components/Signin.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { isUserSignedIn } from 'blockstack';
+const logo = './src/images/logo.png';
+export default class Signin extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { handleSignIn } = this.props;
+
+    return (
+      <div className="panel-landing" id="section-1">
+        <img src={logo} alt={"logo"}/>
+        <h1 className="landing-heading">Welcome to Replay</h1>
+        <p className="lead">
+          <button
+            className="btn btn-primary btn-lg"
+            id="signin-button"
+            onClick={ handleSignIn.bind(this) }
+          >
+            Sign in with Blockstack
+          </button>
+        </p>
+      </div>
+    );
+  }
+}
