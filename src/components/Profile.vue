fc3b37060a5abf6fe1b6284bc83e1ee639d9c319
Cryptocracy <admin@cryptocracy.io>
2019-03-26 15:28:22 -0700

    fix button icon
    fix subscribe to channel button icon

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -23,16 +23,16 @@
                 <v-list-tile-action v-if="$route.params.id !== 'my-profile'" class="ml-auto">
                   <v-tooltip bottom v-if="!isAdded">
                     <v-btn slot="activator" @click.stop="updateContacts(userData, 'addition')" outline fab small color="purple accent-4">
-                      <v-icon  color="purple accent-4">person_add</v-icon>
+                      <v-icon  color="purple accent-4">add_to_queue</v-icon>
                     </v-btn>
-                    <span>Add to Contacts</span>
+                    <span>Subscribe to this Channel</span>
                   </v-tooltip>
 
                   <v-tooltip bottom v-else>
                     <v-btn slot="activator" @click.stop="updateContacts(userData, 'deletion')" outline fab small color="purple accent-4">
                       <v-icon  color="purple accent-4">delete</v-icon>
                     </v-btn>
-                    <span>Remove from Contacts</span>
+                    <span>Unsubscribe from this Channel</span>
                   </v-tooltip>
                 </v-list-tile-action>
 
